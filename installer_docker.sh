947452286880ec88744f7b71f47e54b91164beb7
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2016-02-06 13:45:49 +0800

    Update installer_docker.sh

--- installer_docker.sh
+++ installer_docker.sh
@@ -1,12 +1,10 @@
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-  echo "deb https://apt.dockerproject.org/repo ubuntu-vivid main" > /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get install -y linux-image-extra-$(uname -r)
-sudo apt-get install -y docker-engine
-sudo usermod -a -G docker basedock
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+  echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y linux-image-extra-$(uname -r)
+apt-get install -y docker-engine
+systemctl enable docker
 
-sudo service docker start
-
-sudo groupadd docker
-sudo gpasswd -a ${USER} docker
-sudo service docker restart
+groupadd docker
+gpasswd -a ${USER} docker
+systemctl enable docker
